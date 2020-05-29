7d35c0f8267ccb0204b73a7caaef0ef0c67d7d83
Christian Santos <christian.santos@ibti.org.br>
2020-05-29 20:36:23 -0300

    importando fonts externas

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,11 +3,12 @@ import { Image } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 const SignIn: React.FC = () => {
   return (
     <Container>
-      <Image source={logoImg}></Image>
+      <Image source={logoImg} />
+      <Title>Faça seu logon:</Title>
     </Container>
   );
 };
