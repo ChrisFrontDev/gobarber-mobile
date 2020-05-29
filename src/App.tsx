c4e427a36f97ef1a78db11660da0a9d34a383ff1
Christian Santos <christian.santos@ibti.org.br>
2020-05-29 20:13:58 -0300

    configurado navegacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import 'react-native-gesture-handler';
 
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-      <View style={{ flex: 1, backgroundColor: '#312e38' }} />
-    </>
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Routes></Routes>
+      </View>
+    </NavigationContainer>
   );
 };
 
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+import { Text } from 'react-native';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Text>adopasd</Text>
+    </Container>
+  );
+};
+
+export default SignIn;
