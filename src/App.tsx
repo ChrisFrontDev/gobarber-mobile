7dfab48879af1da2654f37d2501f2de84f41e0d1
Christian Santos <christian.santos@ibti.org.br>
2020-05-29 19:22:54 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+    </>
+  );
+};
+
+export default App;
